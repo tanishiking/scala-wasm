27dba87153a9530da723cfe6580e839710a536c4
Sébastien Doeraene <sjrdoeraene@gmail.com>
2024-04-03 19:42:13 +0200

    Import the Scala.js test suite and enable what works.

---
+++ scalajs-test-suite/src/main/scala/org/scalajs/testsuite/utils/BuildInfo.scala
@@ -0,0 +1,22 @@
+package org.scalajs.testsuite.utils
+
+private[utils] object BuildInfo {
+  final val scalaVersion = "2.12.19"
+  final val hasSourceMaps = false
+  final val isNoModule = false
+  final val isESModule = true
+  final val isCommonJSModule = false
+  final val usesClosureCompiler = false
+  final val hasMinifiedNames = true
+  final val compliantAsInstanceOfs = false
+  final val compliantArrayIndexOutOfBounds = false
+  final val compliantArrayStores = false
+  final val compliantNegativeArraySizes = false
+  final val compliantNullPointers = false
+  final val compliantStringIndexOutOfBounds = false
+  final val compliantModuleInit = false
+  final val strictFloats = true
+  final val productionMode = false
+  final val esVersion = 6
+  final val useECMAScript2015Semantics = true
+}
