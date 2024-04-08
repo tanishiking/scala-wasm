b96945aaef0191eb84d33f92324dcf71178a9907
Sébastien Doeraene <sjrdoeraene@gmail.com>
2024-04-08 15:00:30 +0200

    Merge the `cli` project into the `tests` project.
    The `cli` was only used to link the test suites to be run by the
    `tests` project. We now directly integrate its logic inside the
    test executor of `tests`.
    
    In addition to cleaning things up, this improves error reporting
    in case a suite fails to link: the error will be reported as part
    of the test for that suite, rather than as a global failure ahead
    of time. Failing to link one suite does not prevent the other ones
    from succeeding anymore.
    
    Also update the readme about how to run tests.

--- tests/src/test/scala/tests/CoreTests.scala
+++ tests/src/test/scala/tests/CoreTests.scala
@@ -1,8 +1,18 @@
 package tests
 
+import scala.concurrent.Future
+
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
+
 import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits._
+
+import wasm.WebAssemblyLinkerImpl
+
+import org.scalajs.linker._
+import org.scalajs.linker.interface._
+
+import org.scalajs.logging._
 
 /** The `tests/test` command compiles the Scala sources under `test-suite` into Wasm with static
   * initializers (see `cli/src/main/scala/Main.scala`), where these static initializers should point
@@ -16,12 +26,67 @@ import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits._
   * the Wasm module will execute `unreachable` and fail during instantiation.
   */
 class CoreTests extends munit.FunSuite {
-  cli.TestSuites.suites.map { suite =>
-    test(suite.className) {
-      js.`import`[js.Any](s"../../../../target/${suite.className}/main.mjs").toFuture.map { _ =>
-        ()
-      }
+  import CoreTests._
+
+  private val classpath = {
+    val cpEnvVar = js.Dynamic.global.process.env.SCALAJS_CLASSPATH
+
+    (cpEnvVar: Any) match {
+      case cpEnvVar: String if cpEnvVar != "" =>
+        cpEnvVar.split(';').toList
+      case _ =>
+        throw new IllegalArgumentException("The classpath was not provided.")
     }
   }
 
+  private val globalIRCache = StandardImpl.irFileCache()
+
+  private val linkerConfig = StandardConfig()
+    .withESFeatures(_.withESVersion(ESVersion.ES2016)) // to be able to link `**`
+    .withSemantics(_.optimized) // because that's the only thing we actually support at the moment
+    .withModuleKind(ModuleKind.ESModule)
+    .withOptimizer(false)
+    .withOutputPatterns(OutputPatterns.fromJSFile("%s.mjs"))
+
+  TestSuites.suites.foreach { suite =>
+    test(suite.className) {
+      testOneSuite(suite)
+    }
+  }
+
+  private def testOneSuite(suite: TestSuites.TestSuite): Future[Unit] = {
+    val className = suite.className
+    val methodName = suite.methodName
+
+    val cache = globalIRCache.newCache
+    val irFilesFuture = NodeIRContainer.fromClasspath(classpath).map(_._1).flatMap(cache.cached _)
+
+    val logger = new ScalaConsoleLogger(Level.Error)
+
+    val linker = WebAssemblyLinkerImpl.linker(linkerConfig)
+    val moduleInitializers = List(ModuleInitializer.mainMethod(className, methodName))
+
+    val outputDirRelToPwd = s"./target/$className/"
+    createDir(outputDirRelToPwd)
+    val output = NodeOutputDirectory(outputDirRelToPwd)
+
+    val outputDirRelToMyJSFile = s"../../../../target/$className/"
+
+    for {
+      irFiles <- irFilesFuture
+      linkerResult <- linker.link(irFiles, moduleInitializers, output, logger)
+      runResult <- js.`import`[js.Any](s"$outputDirRelToMyJSFile/main.mjs").toFuture
+    } yield {
+      ()
+    }
+  }
+}
+
+object CoreTests {
+  def createDir(dir: String): Unit =
+    mkdirSync(dir, js.Dynamic.literal(recursive = true))
+
+  @js.native
+  @JSImport("node:fs")
+  def mkdirSync(path: String, options: js.Object = js.native): Unit = js.native
 }
